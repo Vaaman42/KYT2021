ae6eda01fa6857aedc6b412417b373df38d00786
Aurélien Dekesel <47607649+Vaaman42@users.noreply.github.com>
2021-10-09 21:45:47 +0200

    Drag mouse

--- Zuccerverse/Assets/Scripts/GameManager.cs
+++ Zuccerverse/Assets/Scripts/GameManager.cs
@@ -1,9 +1,16 @@
+using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    #region Serialized Fields
+
+    [SerializeField] DialogManager DialogManager;
+    [SerializeField] List<Person> Interviewees;
+
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
